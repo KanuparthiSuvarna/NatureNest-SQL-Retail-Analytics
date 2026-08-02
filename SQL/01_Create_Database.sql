80221ec5c256ee6dedfc8443f0ebf1cf158d9d7b
Kanuparthi Suvarna <suvarnakanuparthi@gmail.com>
2026-08-02 11:33:59 +0530

    Create 01_Create_Database.sql
    Add database creation script

---
+++ SQL/01_Create_Database.sql
@@ -0,0 +1,9 @@
+-- ==========================================
+-- NatureNest SQL Retail Analytics Project
+-- File: 01_Create_Database.sql
+-- Description: Creates the NatureNest database.
+-- ==========================================
+
+CREATE DATABASE IF NOT EXISTS NatureNest;
+
+USE NatureNest;
